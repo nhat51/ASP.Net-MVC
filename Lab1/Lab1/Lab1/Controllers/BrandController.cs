da9c44b107a5d56d5227303db6b7ea1940200740
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-06-17 20:37:18 +0700

    lab1

---
+++ Lab1/Lab1/Lab1/Controllers/BrandController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Brand/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Brand/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brand/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Brand/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Brand/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Brand/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Brand/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
